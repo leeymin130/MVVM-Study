62c38e01b630f159ac0913b619592ec342d78df3
yoomin <leeymin130@gmail.com>
2025-11-04 00:49:43 +0900

    Project setting

---
+++ SwiftfulCrypto/SwiftfulCrypto/SwiftfulCryptoApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftfulCryptoApp.swift
+//  SwiftfulCrypto
+//
+//  Created by yoomin on 11/4/25.
+//
+
+import SwiftUI
+
+@main
+struct SwiftfulCryptoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
