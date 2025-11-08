c36a9d63181231db821bc39158012a9d411ecaf2
yoomin <leeymin130@gmail.com>
2025-11-08 20:01:50 +0900

    feat: add button components to HomeView and implement tap animation

--- SwiftfulCrypto/SwiftfulCrypto/Core/Home/Views/HomeView.swift
+++ SwiftfulCrypto/SwiftfulCrypto/Core/Home/Views/HomeView.swift
@@ -7,6 +7,9 @@
 import SwiftUI
 
 struct HomeView: View {
+    
+    @State private var showPortfolio: Bool = false
+    
     var body: some View {
         ZStack {
             // background layer
@@ -15,7 +18,26 @@ struct HomeView: View {
             
             // content layer
             VStack {
-                Text("Header")
+                HStack {
+                    CircleButtonView(iconName: showPortfolio ? "plus" : "info")
+                        .animation(.none)
+                    Spacer()
+                    Text(showPortfolio ? "Portfolio" : "Live Prices")
+                        .font(.headline)
+                        .fontWeight(.heavy)
+                        .foregroundColor(Color.theme.accent)
+                        .animation(.none)
+                    Spacer()
+                    CircleButtonView(iconName: "chevron.right")
+                        .rotationEffect(Angle(degrees: showPortfolio ? 180 : 0))
+                        .onTapGesture {
+                            withAnimation(.spring()) {
+                                showPortfolio.toggle()
+                            }
+                        }
+                }
+                .padding(.horizontal)
+                
                 Spacer(minLength: 0)
 
             }
