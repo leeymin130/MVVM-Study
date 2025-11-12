6cac050514206073938a298f36a0d5ca4cfe08b9
yoomin <leeymin130@gmail.com>
2025-11-12 12:22:22 +0900

    refactor: clean up CoinModel properties and types
    - Removed unnecessary optional types
    - Changed Int values to Double
    - Deleted unused properties

--- SwiftfulCrypto/SwiftfulCrypto/Models/CoinModel.swift
+++ SwiftfulCrypto/SwiftfulCrypto/Models/CoinModel.swift
@@ -52,20 +52,20 @@ import Foundation
  
  */
 
-struct CoinModel {
-    let id, symbol, name: String?
-    let image: String?
-    let currentPrice, marketCap, marketCapRank, fullyDilutedValuation: Int?
-    let totalVolume, high24H, low24H: Int?
+struct CoinModel {    
+    let id, symbol, name: String
+    let image: String
+    let currentPrice: Double
+    let marketCap, marketCapRank, fullyDilutedValuation: Double?
+    let totalVolume, high24H, low24H: Double?
     let priceChange24H, priceChangePercentage24H: Double?
-    let marketCapChange24H: Int?
+    let marketCapChange24H: Double?
     let marketCapChangePercentage24H: Double?
-    let circulatingSupply, totalSupply, maxSupply, ath: Int?
+    let circulatingSupply, totalSupply, maxSupply, ath: Double?
     let athChangePercentage: Double?
     let athDate: String?
     let atl, atlChangePercentage: Double?
     let atlDate: String?
-    let roi: NSNull?
     let lastUpdated: String?
     let sparklineIn7D: SparklineIn7D?
     let priceChangePercentage24HInCurrency: Double?
