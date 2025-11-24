f82d1517f5ee120683b780c4d2849f1183ea6b39
yoomin <leeymin130@gmail.com>
2025-11-25 02:31:57 +0900

    feat: display coin lists with animations and column titles
    - Display allCoins and portfolioCoins conditionally using ForEach
    - Add slide transition animations for list switching
    - Set list row insets for proper spacing
    - Add columnTitles with alignment matching coin data columns

--- SwiftfulCrypto/SwiftfulCrypto/Core/Home/Views/HomeView.swift
+++ SwiftfulCrypto/SwiftfulCrypto/Core/Home/Views/HomeView.swift
@@ -21,11 +21,17 @@ struct HomeView: View {
             VStack {
                 homeHeader
                 
-                List {
-                    CoinRowView(coin: DeveloperPreview.instance.coin, showHoldingsColumn: false)
+                columnTitles
+                
+                if !showPortfolio {
+                    allCoinsList
+                        .transition(.move(edge: .leading))
                 }
-                .listStyle(PlainListStyle())
-                                
+                if showPortfolio {
+                    portfolioCoinsList
+                        .transition(.move(edge: .trailing))
+                }
+                                                
                 Spacer(minLength: 0)
 
             }
@@ -68,5 +74,40 @@ extension HomeView {
                 }
         }
         .padding(.horizontal)        
+    }
+    
+    private var allCoinsList: some View {
+        List {
+            ForEach(vm.allCoins) { coin in
+                CoinRowView(coin: coin, showHoldingsColumn: false)
+                    .listRowInsets(.init(top: 10, leading: 0, bottom: 10, trailing: 10))
+            }
+        }
+        .listStyle(PlainListStyle())
+    }
+    
+    private var portfolioCoinsList: some View {
+        List {
+            ForEach(vm.portfolioCoins) { coin in
+                CoinRowView(coin: coin, showHoldingsColumn: true)
+                    .listRowInsets(.init(top: 10, leading: 0, bottom: 10, trailing: 10))
+            }
+        }
+        .listStyle(PlainListStyle())
+    }
+    
+    private var columnTitles: some View {
+        HStack {
+            Text("Coin")
+            Spacer()
+            if showPortfolio {
+                Text("Holdings")
+            }
+            Text("Price")
+                .frame(width: UIScreen.main.bounds.width / 3.5, alignment: .trailing)
+        }
+        .font(.caption)
+        .foregroundColor(Color.theme.secondaryText)
+        .padding(.horizontal)
     }
 }
