541813519f6a0f62e5160d3c2998fa56bbac3cc7
yoomin <leeymin130@gmail.com>
2025-12-15 12:06:25 +0900

    refactor: add NetworkManager for reusable Combine network pipeline
    - Encapsulate URLSession setup (subscribe, tryMap, receive) in NetworkManager.download()
    - Extract completion error handling to NetworkManager.handleCompletion
    - CoinDataService now handles only URL construction and model decoding

---
+++ SwiftfulCrypto/SwiftfulCrypto/Utilities/NetworkingManager.swift
@@ -0,0 +1,36 @@
+//
+//  NetworkingManager.swift
+//  SwiftfulCrypto
+//
+//  Created by yoomin on 12/11/25.
+//
+
+import Foundation
+import Combine
+
+class NetworkingManager {
+    
+    static func download(url: URL) -> AnyPublisher<Data, any Error> {
+        return URLSession.shared.dataTaskPublisher(for: url)
+            .subscribe(on: DispatchQueue.global(qos: .default))
+            .tryMap { (output) -> Data in
+                
+                guard let response = output.response as? HTTPURLResponse, response.statusCode >= 200 && response.statusCode < 300 else {
+                    throw URLError(.badServerResponse)
+                }
+                return output.data
+            }
+            .receive(on: DispatchQueue.main)
+            .eraseToAnyPublisher()
+    }
+    
+    static func handleCompletion(completion: Subscribers.Completion<Error>) {
+        switch completion {
+        case .finished:
+            break
+        case .failure(let error):
+            print(error.localizedDescription)
+            
+        }
+    }
+}
